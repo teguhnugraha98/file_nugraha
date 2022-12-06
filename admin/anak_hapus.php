671afcef8f20ff1ea82b4a440c6d8bd9e2f9f4cc
TeguhNugrahaa <38280927+TeguhNugrahaa@users.noreply.github.com>
2022-12-07 06:32:43 +0700

    Update anak_hapus.php

--- admin/anak_hapus.php
+++ admin/anak_hapus.php
@@ -5,4 +5,4 @@ $id  = $_GET['id'];
 mysqli_query($koneksi, "update anak set anak_id='1' where anak_id='$id'");
 
 mysqli_query($koneksi, "delete from anak where anak_id='$id'");
-header("location:datapensiun.php");
+header("location:dataanak.php");
